a50a385bf77c19d2b58be166330a518a7f157e7d
lamdv <lam.dv@live.com>
2017-04-25 11:14:27 +0700

    Init

---
+++ Code/grad_desc_linear_reg.py
@@ -0,0 +1,25 @@
+import numpy as np
+import re
+
+genders = {'male':0, 'female':1}
+ranks = {"full":0, "associate":1, "assistant":2}
+degrees = {"masters":0, "doctorate":1}
+
+with  open('salary.dat', 'r') as input:
+    length = 0
+    A = np.array([[0, 0, 0, 0, 0]])
+    y = np.array([0])
+    for line in input.readlines():
+        wordList = re.sub("[^\w]", " ", line).split()
+        gender = genders[wordList[0]]
+        rank = ranks[wordList[1]]
+        degree = degrees[wordList[3]]
+        vect = np.array([genders[wordList[0]], ranks[wordList[1]], wordList[2], degrees[wordList[3]], wordList[4]]) #gender, rank, yr, degree received, yd
+        A = np.append(A, [vect], axis=0)
+        y = np.append(y, [wordList[5]], axis=0)
+        length += 1
+print(A)
+print(y)
+
+w = np.linalg.lstsq(A, y)[0]
+print(w)

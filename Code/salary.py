Fix type casting, add average error calculation

Code/salary.py
@@ -14,10 +14,17 @@ with  open('salary.dat', 'r') as input:
         gender = genders[wordList[0]]
         rank = ranks[wordList[1]]
         degree = degrees[wordList[3]]
-        vect = np.array([1, genders[wordList[0]], ranks[wordList[1]], wordList[2], degrees[wordList[3]], wordList[4]]) #gender, rank, yr, degree received, yd
+        vect = np.array([1, float(genders[wordList[0]]), ranks[wordList[1]], float(wordList[2]), degrees[wordList[3]], float(wordList[4])]) #gender, rank, yr, degree received, yd
         A = np.append(A, [vect], axis=0)
-        y = np.append(y, [wordList[5]], axis=0)
+        y = np.append(y, [float(wordList[5])], axis=0)
+        length += 1
         
 w = np.linalg.lstsq(A, y)[0]
 print('Least square weight: ')
 print(w)
+err = np.array([0])
+for i in range(0, length-1, 1):
+    print(A[i,:])
+    err = np.append(err, np.dot(A[i,:], w) - y[i])
+
+print("Error: ", np.average(err))
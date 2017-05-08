ab5604a0de467b191ef2f9074a1193917ebc2a5a
lamdv <lam.dv@live.com>
2017-05-08 20:03:29 +0700

    "Implement ELM on Salary Dataset"

--- Code/salary_neural_net.py
+++ Code/salary_neural_net.py
@@ -1,13 +1,14 @@
 import numpy as np
 import re
 
+width = 10
 genders = {'male':0, 'female':1}
 ranks = {"full":0, "associate":1, "assistant":2}
 degrees = {"masters":0, "doctorate":1}
 
 with  open('salary.dat', 'r') as input:
     length = 0
-    A = np.array([[0, 0, 0, 0, 0, 0]])
+    A = np.array([[0, 0, 0, 0, 0, 0]]) #input layer size of 6
     y = np.array([0])
     for line in input.readlines():
         wordList = re.sub("[^\w]", " ", line).split()
@@ -18,13 +19,33 @@ with  open('salary.dat', 'r') as input:
         A = np.append(A, [vect], axis=0)
         y = np.append(y, [float(wordList[5])], axis=0)
         length += 1
-        
-w = np.linalg.lstsq(A, y)[0]
-print('Least square weight: ')
-print(w) 
+
+#sigmoid function
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+#sigmoid derivative function
+def deriv(x):
+    return x*(1-x)
+
+np.random.seed()
+
+syn0 = 2*np.random.random((6,width)) - 1
+
+#for iter in range(10000):
+l0 = A #input layer
+#feed forward into the network
+l1 = np.array([sigmoid(np.dot(l0, syn0[:,1]))]) 
+for iter_width in range(width-1):
+    l1 = np.append(l1, [sigmoid(np.dot(l0, syn0[:,iter_width]))],axis = 0) # Single (width) wide feed forward perceptron with randomized bias
+
+#print (syn0)
+#print (l1.T)
+#print(A, y)
+w = np.linalg.lstsq(l1.T, y)[0] #least square learning on the output weight of random layer
+
 err = np.array([0])
 for i in range(0, length-1, 1):
-    print(A[i,:])
-    err = np.append(err, np.dot(A[i,:], w) - y[i])
+    err = np.append(err, np.dot(l1.T[i,:], w) - y[i])
 
 print("Error: ", np.average(err))
